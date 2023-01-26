18e0b6de8e8c8e34e0b90cc852d0d295fa4036c1
FabricioIkehara <fah.hiroshi@gmail.com>
2023-01-26 12:27:02 -0200

    exercicios de java

---
+++ ExerciciosJava1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exerciciosjava1;
+
+/**
+ *
+ * @author Fabrício
+ */
+public class ExerciciosJava1 {
+
+    public static void main(String[] args) {
+        System.out.println("Seja bem vindo ao meu programa!!");
+        //Calcule o produto de 8 por 90:
+        System.out.print("O produto de 8 por 90 é:");
+        System.out.println(8*90);
+        //Calcule o quociente de 90 por 30;
+        System.out.println("Calcule o quociente de 90 por 30:" +90/30);
+        //Calcular o quadrado de 9:
+        System.out.println("O quadrado de 9 é:" +9*9);
+    
+    }
+}
